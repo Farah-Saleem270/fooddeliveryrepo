5a046b76134b9fc883b6e3bad0fe185eebefdaba
Farah Saleem <fs8578058@gmail.com>
2026-03-18 05:11:56 -0700

    add some new code

--- main.js
+++ main.js
@@ -5,3 +5,22 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: "#prev",
     },
 });
+
+const cartIcon = document.querySelector(".cart-icon");
+const cartTab = document.querySelector(".cart-tab");
+const closeBtn = document.querySelector(".close-btn");
+
+cartIcon.addEventListener("click", ()=> {
+    cartTab.classList.add("cart-tab-active");
+   closeBtn.addEventListener("click",()=> {
+     cartTab.classList.remove("cart-tab-active");
+   })
+})
+
+let productsList =[];
+
+const initApp = ()=> {
+  fetch("pr")
+}
+
+initApp();
