c0a17032cbb7b98ae40c973b667bd653a0c3ab80
Farah Saleem <fs8578058@gmail.com>
2026-04-12 03:37:59 -0700

    add some new code of line

--- main.js
+++ main.js
@@ -1,37 +1,59 @@
 var swiper = new Swiper(".mySwiper", {
-    loop: true,
-    navigation: {
-        nextEl: "#next",
-        prevEl: "#prev",
-    },
+  loop: true,
+  navigation: {
+    nextEl: "#next",
+    prevEl: "#prev",
+  },
 });
 
 const cartIcon = document.querySelector(".cart-icon");
 const cartTab = document.querySelector(".cart-tab");
 const closeBtn = document.querySelector(".close-btn");
+const cardList = document.querySelector(".card-list");
 
-cartIcon.addEventListener("click", ()=> {
-    cartTab.classList.add("cart-tab-active");
-   closeBtn.addEventListener("click",()=> {
-     cartTab.classList.remove("cart-tab-active");
-   })
+cartIcon.addEventListener("click", () => {
+  cartTab.classList.add("cart-tab-active");
+  closeBtn.addEventListener("click", () => {
+    cartTab.classList.remove("cart-tab-active");
+  })
 })
 
-let productsList =[];
+let productsList = [];
 
 const showCards = () => {
+  productsList.forEach(product => {
+    const orderCard = document.createElement("div");
+    orderCard.classList.add('order-card');
+    orderCard.innerHTML = `
+    <div class="card-img">
+    <img src="${product.image}">
+    </div>
+    <h4>${product.name}</h4>
+    <h4 class="price">${product.price}</h4>
+    <a href="#" class="btn card-btn">Add to Card</a>
+  `;
+    cardList.appendChild(orderCard);
+ 
+    const cardBtn = orderCard.document.querySelector(".card-btn");
+    cardBtn.addEventListener("click", ()=> {
+      alert("hi");
+    });
 
+
+
+
+  })
 }
 
-const initApp = ()=> {
+const initApp = () => {
   fetch("products.json").then
-  (response => response.json()).then
-  (data  => {
-    productsList = data;
-  showCards();
-    
-  })
-  
+    (response => response.json()).then
+    (data => {
+      productsList = data;
+      showCards();
+
+    })
+
 }
 
 initApp();
